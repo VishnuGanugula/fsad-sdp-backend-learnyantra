65f343788f884e51fb32dc9189bac18932bea613
Dheeraj32903 <2400032903@kluniversity.in>
2026-03-30 19:31:35 +0530

    loginverification

--- src/main/java/com/klef/fsad/sdp/SpringBootSdplmsBackendProjectApplication.java
+++ src/main/java/com/klef/fsad/sdp/SpringBootSdplmsBackendProjectApplication.java
@@ -6,13 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSdplmsBackendProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSdplmsBackendProjectApplication.class, args);
-<<<<<<< HEAD
-		System.out.println("Spring Boot SPDLMS Backend Project");
-=======
-		System.out.println("Spring Boot Project is running");
->>>>>>> 43419e9 (authentication)
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSdplmsBackendProjectApplication.class, args);
 
+        System.out.println("Spring Boot SSDLMS Backend Project");
+      
+    }
 }
--- src/main/java/com/klef/fsad/sdp/controller/AdminController.java
+++ src/main/java/com/klef/fsad/sdp/controller/AdminController.java
@@ -5,13 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.klef.fsad.sdp.entity.Admin;
 import com.klef.fsad.sdp.service.AdminService;
-
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("adminapi")
