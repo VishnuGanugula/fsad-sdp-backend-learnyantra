832aebd5e1aa1b714f06c209a9361bd829222529
VishnuGanugula <vishnuganugula@gmail.com>
2026-03-31 08:37:42 +0530

    Changes in admin

--- src/main/java/com/klef/fsad/sdp/service/AdminService.java
+++ src/main/java/com/klef/fsad/sdp/service/AdminService.java
@@ -1,7 +1,24 @@
 package com.klef.fsad.sdp.service;
 
+import java.util.List;
+
 import com.klef.fsad.sdp.entity.Admin;
+import com.klef.fsad.sdp.entity.Instructor;
+import com.klef.fsad.sdp.entity.Student;
 
 public interface AdminService {
+	
 	public Admin verifyAdminLogin(String username,String password);
+	
+	public String addInstructor(Instructor instructor);
+    public List<Instructor> viewAllInstructors();
+    public boolean deleteInstructor(int id); 
+    
+    // Student Management
+    public List<Student> viewAllStudents();
+    public String deleteStudent(int id); 
+    
+    // Dashboard Status
+    public long getStudentCount();
+    public long getInstructorCount();
 }
